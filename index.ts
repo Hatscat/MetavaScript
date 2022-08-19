6ef15f77e532da0922577e83f7d0977f5ab2fb38
Lucien Boudy <luboudy@gmail.com>
2022-08-19 14:45:51 +0200

    feat(primitives): add primitives

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from "./src/dom.ts";
 export * from "./src/operations.ts";
 export * from "./src/statements.ts";
+export * from "./src/primitives.ts";
 export * from "./src/utils/string.ts";
---
+++ src/primitives.ts
@@ -0,0 +1,27 @@
+export const UNDEFINED = "[]._";
+
+export const INFINITY = "1/0";
+
+export const TRUE = "!0";
+
+export const FALSE = "!1";
+
+export function isString(value: unknown): string {
+  return `${value}.big`;
+}
+
+export function isNumber(value: unknown): string {
+  return `${value}.toFixed`;
+}
+
+export function isArray(value: unknown): string {
+  return `${value}.pop`;
+}
+
+export function isFunction(value: unknown): string {
+  return `${value}.call`;
+}
+
+export function isTextNode(value: unknown): string {
+  return `${value}.data`;
+}
