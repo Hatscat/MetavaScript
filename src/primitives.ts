6ccf62f35fe6c409c127cba87e0e5cf2ca2f8769
Lucien Boudy <luboudy@gmail.com>
2022-08-26 21:31:48 +0200

    feat(primitives): fix some functions

--- src/primitives.ts
+++ src/primitives.ts
@@ -7,21 +7,21 @@ export const TRUE = 1;
 export const FALSE = 0;
 
 export function isString(value: unknown): string {
-  return `${value}.big`;
+  return `(${value}).big`;
 }
 
 export function isNumber(value: unknown): string {
-  return `${value}.toFixed`;
+  return `(${value}).toFixed`;
 }
 
 export function isArray(value: unknown): string {
-  return `${value}.pop`;
+  return `(${value}).pop`;
 }
 
 export function isFunction(value: unknown): string {
-  return `${value}.call`;
+  return `(${value}).call`;
 }
 
 export function isTextNode(value: unknown): string {
-  return `${value}.data`;
+  return `(${value}).data`;
 }
