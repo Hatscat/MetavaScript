55e244e793c8d5d6ed583351129518ebc75c37b2
Lucien Boudy <luboudy@gmail.com>
2022-09-02 14:16:00 +0200

    chore(index): remove utils export

--- index.ts
+++ index.ts
@@ -2,4 +2,3 @@ export * from "./src/dom.ts";
 export * from "./src/operations.ts";
 export * from "./src/statements.ts";
 export * from "./src/primitives.ts";
-export * from "./src/utils/string.ts";
