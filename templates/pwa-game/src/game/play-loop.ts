29dbc2c7bf2be93247f5ea385079132657bf8394
Lucien Boudy <luboudy@gmail.com>
2023-03-06 21:19:33 +0100

    feat(template): draw player and target

--- templates/pwa-game/src/game/play-loop.ts
+++ templates/pwa-game/src/game/play-loop.ts
@@ -1,3 +1,4 @@
+import { config } from "../config.ts";
 import { assign, execFunc, prop, statements, Text } from "../deps.ts";
 import { canvasContext, state } from "../variables.ts";
 
@@ -5,9 +6,9 @@ export function playLoop(): string {
   return statements(
     // TODO: rules + actions first there
     drawBackground(),
-    // drawTarget(),
+    drawTarget(),
     // drawBullets(),
-    // drawPlayer(),
+    drawPlayer(),
   );
 }
 
@@ -19,6 +20,34 @@ function drawBackground(): string {
       0,
       state.canvas.width,
       state.canvas.height,
+    ]),
+  );
+}
+
+function drawTarget(): string {
+  return statements(
+    assign(
+      prop(canvasContext, "font"),
+      Text(`${config.target.radius * 2}px A`),
+    ),
+    execFunc(prop(canvasContext, "fillText"), [
+      config.target.icon,
+      state.target.pos.x,
+      state.target.pos.y,
+    ]),
+  );
+}
+
+function drawPlayer(): string {
+  return statements(
+    assign(
+      prop(canvasContext, "font"),
+      Text(`${config.player.radius * 2}px A`),
+    ),
+    execFunc(prop(canvasContext, "fillText"), [
+      config.player.icon,
+      state.player.pos.x,
+      state.player.pos.y,
     ]),
   );
 }
