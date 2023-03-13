09377599f33074109c1583c7b386265f6d2d432f
Lucien Boudy <luboudy@gmail.com>
2023-03-13 19:16:08 +0100

    feat(templates): add a http server for dev cmd

--- templates/pwa-game/main.ts
+++ templates/pwa-game/main.ts
@@ -1,4 +1,17 @@
-import { writeHtmlBundle } from "./src/deps.ts";
+import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.178.0/http/file_server.ts";
+import { loadPublicFiles, writeHtmlBundle } from "./src/deps.ts";
 import { getGameSrc } from "./src/index.ts";
 
-writeHtmlBundle(getGameSrc());
+await Promise.all([
+  loadPublicFiles(),
+  writeHtmlBundle(getGameSrc()),
+]);
+
+if (Deno.args[0] === "serve") {
+  serve((req) => {
+    return serveDir(req, {
+      fsRoot: "dist",
+    });
+  });
+}
--- templates/pwa-game/run.sh
+++ templates/pwa-game/run.sh
@@ -3,11 +3,11 @@
 case "$1" in
 
   "build")
-    deno run --check --allow-write main.ts
+    deno run --check --allow-read --allow-write main.ts
     ;;
 
   "dev")
-    deno run --check --watch --allow-write main.ts
+    deno run --check --watch --allow-read --allow-write --allow-net main.ts serve
     ;;
 
   "reload-deps")
