dee76fda874c31400a7f20485bddcb0e972d2111
Lucien Boudy <luboudy@gmail.com>
2023-03-13 21:19:02 +0100

    feat(pwa): add some helpers for web app

--- src/pwa.ts
+++ src/pwa.ts
@@ -9,3 +9,20 @@ export function registerServiceWorker(path = Text("/sw.js")): string {
 export function manifestLink(path = "m.webmanifest"): string {
   return element("link", { tagProps: { rel: "manifest", href: path } });
 }
+
+export function viewportMeta(
+  content = "width=device-width,initial-scale=1",
+): string {
+  return element("meta", { tagProps: { name: "viewport", content } });
+}
+
+export function htmlDoctype(lang = "en"): string {
+  return [
+    element("!DOCTYPE", { tagProps: { html: undefined } }),
+    element("html", { tagProps: { lang } }),
+  ].join("");
+}
+
+export function titleTag(title: string): string {
+  return element("title", { children: title, closed: true });
+}
