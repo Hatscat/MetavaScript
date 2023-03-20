e6f7eae038a1bb03d0a81281dd8caccb10fdd244
Lucien Boudy <luboudy@gmail.com>
2023-03-20 21:48:49 +0100

    docs(data-flow): add doc comments

--- src/data-flow.ts
+++ src/data-flow.ts
@@ -1,8 +1,46 @@
+/**
+ * an interface to extends for typed actions related to the State Mutator pattern
+ */
 export interface ActionBase<ActionType = string | number> {
   type: ActionType;
   payload?: { [key: string]: unknown };
 }
 
+/**
+ * helper to create the `dispatch(action)` function, in the context of the State Mutator pattern
+ * @example
+ * const state = {
+ *   player: {
+ *     x: "px",
+ *     y: "py",
+ *   },
+ * };
+ *
+ * type Action = {
+ *   type: "jump";
+ * } | {
+ *   type: "move";
+ *   payload: {
+ *     direction: "left" | "right";
+ *   };
+ * };
+ *
+ * const mutator = (st: typeof state, action: Action): string => {
+ *   switch (action.type) {
+ *     case "jump":
+ *       return decrement(st.player.y);
+ *     case "move":
+ *       return increment(st.player.x);
+ *   }
+ * };
+ *
+ * const dispatch = createActionDispatch(state, mutator);
+ *
+ * // returns "py--"
+ * dispatch({ type: "jump" });
+ * // returns "px++"
+ * dispatch({ type: "move", payload: { direction: "right" } });
+ */
 export function createActionDispatch<
   ActionType,
   State extends { [key: string]: unknown },
