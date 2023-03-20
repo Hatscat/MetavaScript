e843c6f3043deb9f1f968c6cff07e2d2022ccbe6
Lucien Boudy <luboudy@gmail.com>
2023-03-20 21:59:52 +0100

    docs(pwa): add doc comments

--- src/pwa.ts
+++ src/pwa.ts
@@ -2,20 +2,44 @@ import { element } from "./dom.ts";
 import { Text } from "./primitives.ts";
 import { execFunc, prop } from "./statements.ts";
 
+/**
+ * helper to generate the function to register your service worker
+ * @example
+ * // returns "navigator.serviceWorker?.register('/sw.js')"
+ * registerServiceWorker()
+ */
 export function registerServiceWorker(path = Text("/sw.js")): string {
   return execFunc(prop("navigator", "serviceWorker?", "register"), path);
 }
 
+/**
+ * helper to generate the HTML Element that link your web manifest
+ * @example
+ * // returns "<link rel=manifest href=m.webmanifest>"
+ * manifestLink()
+ */
 export function manifestLink(path = "m.webmanifest"): string {
   return element("link", { tagProps: { rel: "manifest", href: path } });
 }
 
+/**
+ * helper to generate the viewport meta tag
+ * @example
+ * // returns "<meta name=viewport content=width=device-width,initial-scale=1>"
+ * viewportMeta()
+ */
 export function viewportMeta(
   content = "width=device-width,initial-scale=1",
 ): string {
   return element("meta", { tagProps: { name: "viewport", content } });
 }
 
+/**
+ * helper to generate the HTML Doctype and the html tag
+ * @example
+ * // returns "<!DOCTYPE html><html lang=en>"
+ * htmlDoctype()
+ */
 export function htmlDoctype(lang = "en"): string {
   return [
     element("!DOCTYPE", { tagProps: { html: undefined } }),
@@ -23,6 +47,12 @@ export function htmlDoctype(lang = "en"): string {
   ].join("");
 }
 
+/**
+ * helper to generate the title tag
+ * @example
+ * // returns "<title>example</title>"
+ * titleTag("example")
+ */
 export function titleTag(title: string): string {
   return element("title", { children: title, closed: true });
 }
